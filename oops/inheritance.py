90f442bb332eb4a93802f6c268ae5a7934f04efb
unknown <shivatiwari7580@gmail.com>
2026-05-25 11:56:11 +0530

    frist commit

--- oops/inheritance.py
+++ oops/inheritance.py
@@ -43,7 +43,7 @@ class bata(factory):
 rahul = bata()
 rahul.display """     
 
-#2. Multiple Inheritane -> 2 Parent, 1 Child
+"""#2. Multiple Inheritane -> 2 Parent, 1 Child
 class Father: #Parent1
 
     def _init_(self):
@@ -69,6 +69,79 @@ class Child(Mother,Father): #Child
 
 obj = Child()
 obj.greet_father()
-obj.greet_mother()
+obj.greet_mother()"""
+
+"""#multilevel inheritence -> one child class beacome parent classs of another class 
+class A:
+    def greet(self):
+        print('this is class A')
+
+class B(A):
+    def show(self):
+        print('this is class B')
+
+class C(B):
+    def details(self):
+        print('this is class C')   
+
+obj = C()
+obj.show()
+obj.greet() """ 
 
 
+"""class CEO:
+    def __init__(self):
+        print('this is ceo class constructor')
+
+class manager(CEO):
+    def __init__(self):
+        super().__init__()
+        print('this is manager class constructor')
+
+class empolyee(manager):
+    def __init__(self):
+        super().__init__()
+        print('this is empolyee class constructor')   
+
+obj = empolyee()"""
+
+# hierarchial inheritance
+"""class parent:
+    def greet(self):
+        print('this is parent class')
+
+class child1(parent):
+    pass 
+
+class child2(parent):
+    pass
+
+obj = child1()
+obj.greet()
+
+obj2 = child2()
+obj2.greet()"""
+
+"""
+EX:2
+class Account:
+    def _init_(self,name,balance):
+        self.name = name
+        self.balance = balance
+    
+    def details(self):
+        print(f"Hello {self.name} you have {self.balance}")
+    
+class Saving(Account):
+    def _init_(self,name,balance):
+        super()._init_(name,balance)
+        print(f'This is Saving class constructor {self.name} , {self.balance}')
+
+class Current(Account):
+    def _init_(self,name,balance,type):
+        super()._init_(name,balance)
+        self.type = type
+        print(f'This is Current class Constructor {self.name} , {self.balance}, {self.type}')
+
+obj = Current("Mukesh",0,"Current")
+"""
